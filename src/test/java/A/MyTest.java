c5b7a69371f0061c934ec30d39a6d41ba917131d
jakubChwesiuk <74013471+jakubChwesiuk@users.noreply.github.com>
2021-08-09 10:45:23 +0200

    Initial commit

---
+++ src/test/java/A/MyTest.java
@@ -0,0 +1,19 @@
+package A;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class MyTest {
+    
+    @Test
+    public void google(){
+        WebDriverManager.chromedriver().setup();
+        
+//        System.setProperty("webdriver.chrome.driver","C:\\ChromeDriver\\92\\chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://google.pl");
+        webDriver.close();
+    }
+    
+}
