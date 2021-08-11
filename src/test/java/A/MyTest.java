dfcf700e1dabe57ad8f5b840f147b875f62d3a31
justcasualguy <kubachw@gmail.com>
2021-08-11 13:54:07 +0200

    Run chrome headless

--- src/test/java/A/MyTest.java
+++ src/test/java/A/MyTest.java
@@ -3,15 +3,17 @@ package A;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 public class MyTest {
     
     @Test
     public void google(){
         WebDriverManager.chromedriver().setup();
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
 //        System.setProperty("webdriver.chrome.driver","C:\\ChromeDriver\\92\\chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
+        WebDriver webDriver = new ChromeDriver(options);
         webDriver.get("https://google.pl");
         webDriver.close();
     }
